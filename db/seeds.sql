5306dacca40de2607ab241fea70a2117ad7e7d69
aujones12 <46331537+aujones12@users.noreply.github.com>
2019-06-15 13:42:59 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 1', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 1', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 2', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 3', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 4', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger 5', true);
